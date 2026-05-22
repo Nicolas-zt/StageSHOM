1061020452e35e61183ac9a2b1468aaae1636792
Nicolas.zito <nicolas.zito@stagiaires.shom.fr>
2026-05-22 11:28:07 +0200

    remise à jour du code de comparaison des résultats

--- TestSpotgins/ResultsCompar.py
+++ TestSpotgins/ResultsCompar.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import numpy as np
-
+import os
 
 def Compar(f1,f2):
     
@@ -9,10 +9,40 @@ def Compar(f1,f2):
     ratio = np.sum(compar)/f1.size*100
     
     return ratio
+
+def Diff(df_list,ref):
     
+    for df in df_list:
+    
+        df.iloc[:,3:] = np.abs(df.iloc[:,3:] - ref.iloc[:,3:])
+        
+    return df_list
+
+def Open(dir_path):
+    List = []
+    for (root,dirs,file) in os.walk(dir_path):
+        for d in dirs:
+            print(d)
+            for f in os.listdir(root + '/' + d):
+                if f.endswith('.IPPP'):
+                    path = f"{root}/{d}/{f}"
+                    
+                    List.append(pd.read_csv(path,comment = "#",delimiter = "\s+",header = None,names = cols))
+                    
+    return List
 if __name__ == "__main__":
     
-    data = pd.read_csv("ALBH00CAN.enu",comment = "#",delimiter = "\s+",header = None,usecols=[0,1,2,3,4,5,6])
-    data2 = pd.read_csv("SPOTGINS_ALBH00CAN.enu",comment = "#",delimiter = "\s+",header = None,usecols=[0,1,2,3,4,5,6])
+    cols = ["# type","calendar  epoch","julian days(1950)","correction (X or lat)",
+            "position (X or lat)","correction (Y or lon)","position (Y or lon)","correction (Z or h)",
+            "position (Z or h)","cov11","cov22","cov33","cov21","cov31","cov32"]
+    data = pd.read_csv("../GinsResults/ALBH00CAN_R_20250010000_01D_30S_MO.rnx.yml.260504_094832.260504_114926.gins.PPP",comment = "#",delimiter = "\s+",header = None,names = cols)
+    data2 = pd.read_csv("../GinsResults/ALBH00CAN_R_20250010000_01D_30S_MO.rnx.yml.260504_095248.260504_115338.gins.IPPP",comment = "#",delimiter = "\s+",header = None,names = cols)
     
-    test = Compar(data,data2)
+    
+    ### Ouverturedes résultats Gins
+    GinsResults = Open('../GinsResults')
+    
+    ### Comparaison avec la ref SHOM
+    Diff_list = Diff(GinsResults, data)
+    
+    
